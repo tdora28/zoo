b3ed0af421400fc59953e99f5f8434cb2721ad49
Dóra Tokai <tokaidora88@gmail.com>
2023-11-01 13:07:16 +0200

    Add card designs with placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,58 @@
+import { useState } from 'react';
+import Header from './Header.jsx';
+import Card from './Card.jsx';
+import Footer from './Footer.jsx';
+
 function App() {
-  return <></>;
+  const [zoo, setZoo] = useState([
+    {
+      id: 1,
+      type: 'animal',
+      name: 'fox',
+      rating: 100,
+      img: 'https://images.pexels.com/photos/158340/fuchs-wild-animal-predator-animal-world-158340.jpeg',
+    },
+    {
+      id: 2,
+      type: 'animal',
+      name: 'rabbit',
+      rating: -5,
+      img: 'https://images.pexels.com/photos/2476390/pexels-photo-2476390.jpeg',
+    },
+    {
+      id: 3,
+      type: 'animal',
+      name: 'panda',
+      rating: 84,
+      img: 'https://images.pexels.com/photos/1661535/pexels-photo-1661535.jpeg',
+    },
+    {
+      id: 4,
+      type: 'bird',
+      name: 'sparrow',
+      rating: 0,
+      img: 'https://images.pexels.com/photos/2662434/pexels-photo-2662434.jpeg',
+    },
+    {
+      id: 5,
+      type: 'animal',
+      name: 'cat',
+      rating: 28,
+      img: 'https://images.pexels.com/photos/7149465/pexels-photo-7149465.jpeg',
+    },
+  ]);
+
+  return (
+    <>
+      <Header />
+      <div className="cards">
+        {zoo.map((item) => {
+          return <Card key={item.id} name={item.name} rating={item.rating} img={item.img} />;
+        })}
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,26 @@
+const Card = (props) => {
+  return (
+    <div className="card">
+      <button className="card__close">
+        <span className="material-symbols-outlined">close</span>
+      </button>
+      <img className="card__img" src={props.img} alt="" />
+      <div className="card__content">
+        <h2 className="card__title">{props.name}</h2>
+        <div className="card__rating">
+          <button>
+            <span className="material-symbols-outlined">remove</span>
+          </button>
+          <p>
+            <span className="material-symbols-outlined">{props.rating >= 0 ? 'favorite' : 'heart_broken'}</span> <span>{props.rating}</span>
+          </p>
+          <button>
+            <span className="material-symbols-outlined">add</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
