baaa59b129c4d58c5cfb8e745b72907a22d39423
tdora28 <tokaidora88@gmail.com>
2023-11-02 15:54:25 +0200

    Connect animals from animalList to the state

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
         <span className='material-symbols-outlined'>close</span>
       </button>
       <div className='card__img'>
-        <img src={props.img} alt='' />
+        <img src='https://images.pexels.com/photos/7149465/pexels-photo-7149465.jpeg' alt='' />
       </div>
       <div className='card__content'>
         <h2 className='card__title'>{props.name}</h2>
@@ -14,7 +14,7 @@ const Card = (props) => {
             <span className='material-symbols-outlined'>remove</span>
           </button>
           <p>
-            <span className='material-symbols-outlined'>{props.rating >= 0 ? 'favorite' : 'heart_broken'}</span> <span>{props.rating}</span>
+            <span className='material-symbols-outlined'>{props.likes >= 0 ? 'favorite' : 'heart_broken'}</span> <span>{props.likes}</span>
           </p>
           <button>
             <span className='material-symbols-outlined'>add</span>
