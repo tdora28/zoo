38698b711b336c8260781631b614499f8cea456c
tdora28 <tokaidora88@gmail.com>
2023-11-02 15:20:04 +0200

    Correct card layout problems with image

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,21 +1,23 @@
 const Card = (props) => {
   return (
-    <div className="card">
-      <button className="card__close">
-        <span className="material-symbols-outlined">close</span>
+    <div className='card'>
+      <button className='card__close'>
+        <span className='material-symbols-outlined'>close</span>
       </button>
-      <img className="card__img" src={props.img} alt="" />
-      <div className="card__content">
-        <h2 className="card__title">{props.name}</h2>
-        <div className="card__rating">
+      <div className='card__img'>
+        <img src={props.img} alt='' />
+      </div>
+      <div className='card__content'>
+        <h2 className='card__title'>{props.name}</h2>
+        <div className='card__rating'>
           <button>
-            <span className="material-symbols-outlined">remove</span>
+            <span className='material-symbols-outlined'>remove</span>
           </button>
           <p>
-            <span className="material-symbols-outlined">{props.rating >= 0 ? 'favorite' : 'heart_broken'}</span> <span>{props.rating}</span>
+            <span className='material-symbols-outlined'>{props.rating >= 0 ? 'favorite' : 'heart_broken'}</span> <span>{props.rating}</span>
           </p>
           <button>
-            <span className="material-symbols-outlined">add</span>
+            <span className='material-symbols-outlined'>add</span>
           </button>
         </div>
       </div>
