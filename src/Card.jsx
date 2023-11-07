bd9a792276d467af74c9b3d2f4383e4f962aabfa
Dóra Tokai <tokaidora88@gmail.com>
2023-11-07 13:44:14 +0200

    Add destructuring in Card

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,23 +1,23 @@
-const Card = (props) => {
+const Card = ({ name, likes }) => {
   return (
-    <div className='card'>
-      <button className='card__close'>
-        <span className='material-symbols-outlined'>close</span>
+    <div className="card">
+      <button className="card__close">
+        <span className="material-symbols-outlined">close</span>
       </button>
-      <div className='card__img'>
-        <img src='https://images.pexels.com/photos/7149465/pexels-photo-7149465.jpeg' alt='' />
+      <div className="card__img">
+        <img src="https://images.pexels.com/photos/7149465/pexels-photo-7149465.jpeg" alt="test image" />
       </div>
-      <div className='card__content'>
-        <h2 className='card__title'>{props.name}</h2>
-        <div className='card__rating'>
+      <div className="card__content">
+        <h2 className="card__title">{name}</h2>
+        <div className="card__rating">
           <button>
-            <span className='material-symbols-outlined'>remove</span>
+            <span className="material-symbols-outlined">remove</span>
           </button>
           <p>
-            <span className='material-symbols-outlined'>{props.likes >= 0 ? 'favorite' : 'heart_broken'}</span> <span>{props.likes}</span>
+            <span className="material-symbols-outlined">{likes >= 0 ? 'favorite' : 'heart_broken'}</span> <span>{likes}</span>
           </p>
           <button>
-            <span className='material-symbols-outlined'>add</span>
+            <span className="material-symbols-outlined">add</span>
           </button>
         </div>
       </div>
