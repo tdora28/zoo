76d65aabe9eeb932aff2c6233ac07ba70e523f77
Dóra Tokai <tokaidora88@gmail.com>
2023-11-21 14:48:32 +0200

    Add text to home cards

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,17 +1,18 @@
-import { Link } from 'react-router-dom';
-import fox from '../assets/fox-pexels-pixabay-162291.jpg';
-import owl from '../assets/owl-pexels-aa-dil-2474014.jpg';
+import { Link } from "react-router-dom";
+import fox from "../assets/fox-pexels-pixabay-162291.jpg";
+import owl from "../assets/owl-pexels-aa-dil-2474014.jpg";
 
 function Home() {
   return (
     <main className="home-main">
-      <h1 className="home-title">Zoo!</h1>
       <div className="home-cards">
         <Link className="home-card" to="/animals">
           <img src={fox} alt="Photo of a fox" />
+          <p>Animals</p>
         </Link>
         <Link className="home-card" to="/birds">
           <img src={owl} alt="Photo of an owl" />
+          <p>Birds</p>
         </Link>
       </div>
     </main>
