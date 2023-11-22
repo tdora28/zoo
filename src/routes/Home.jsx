011c58bfe6ab97f6209aa0ebee3a95f1f208b652
Dóra Tokai <tokaidora88@gmail.com>
2023-11-22 13:09:20 +0200

    Redesign Home to match prototype

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,20 +1,14 @@
 import { Link } from "react-router-dom";
-import fox from "../assets/fox-pexels-pixabay-162291.jpg";
-import owl from "../assets/owl-pexels-aa-dil-2474014.jpg";
 
 function Home() {
   return (
     <main className="home-main">
-      <div className="home-cards">
-        <Link className="home-card" to="/animals">
-          <img src={fox} alt="Photo of a fox" />
-          <p>Animals</p>
-        </Link>
-        <Link className="home-card" to="/birds">
-          <img src={owl} alt="Photo of an owl" />
-          <p>Birds</p>
-        </Link>
-      </div>
+      <Link className="home-card home-card--animals" to="/animals">
+        <p>Animals</p>
+      </Link>
+      <Link className="home-card home-card--birds" to="/birds">
+        <p>Birds</p>
+      </Link>
     </main>
   );
 }
