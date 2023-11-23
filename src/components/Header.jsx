ef2d6dd533e313640d4ae941b8b7c727e2989335
tdora28 <tokaidora88@gmail.com>
2023-11-23 13:56:33 +0200

    Fix CategoryPage layout to fit prototype

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,38 @@
-import { Link } from "react-router-dom";
-import { NavLink } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = ({ resetSearch }) => {
   return (
     <header>
-      <Link to="/" className="header-logo" onClick={resetSearch}>
-        Zoo!
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/" onClick={resetSearch}>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/animals" onClick={resetSearch}>
-              Animals
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/birds" onClick={resetSearch}>
-              Birds
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/about" onClick={resetSearch}>
-              About
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="header-wrap">
+        <Link to="/" className="header-logo" onClick={resetSearch}>
+          Zoo!
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/" onClick={resetSearch}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/animals" onClick={resetSearch}>
+                Animals
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/birds" onClick={resetSearch}>
+                Birds
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" onClick={resetSearch}>
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
--- src/routes/CategoryPage.jsx
+++ src/routes/CategoryPage.jsx
@@ -1,7 +1,7 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
-import Card from "../components/Card.jsx";
-import SearchBar from "../components/SearchBar.jsx";
+import Card from '../components/Card.jsx';
+import SearchBar from '../components/SearchBar.jsx';
 
 function CategoryPage(props) {
   let urlParams = useParams();
@@ -16,7 +16,16 @@ function CategoryPage(props) {
               return el.name.toLowerCase().includes(props.search.toLowerCase());
             })
             .map((el) => {
-              return <Card key={el.name} {...el} category={category} removeCard={() => props.removeCard(el.name, category)} decreaseLikes={() => props.modifyLikes(el.name, "decrease", category)} increaseLikes={() => props.modifyLikes(el.name, "increase", category)} />;
+              return (
+                <Card
+                  key={el.name}
+                  {...el}
+                  category={category}
+                  removeCard={() => props.removeCard(el.name, category)}
+                  decreaseLikes={() => props.modifyLikes(el.name, 'decrease', category)}
+                  increaseLikes={() => props.modifyLikes(el.name, 'increase', category)}
+                />
+              );
             })}
         </>
       );
@@ -26,10 +35,15 @@ function CategoryPage(props) {
   }
 
   return (
-    <section className="cards-wrap">
-      <SearchBar searchHandler={props.searchHandler} />
-      <div className="cards">{displayCards(props.zoo[category].filter((el) => el.name.toLowerCase().includes(props.search.toLowerCase())).length)}</div>
-    </section>
+    <>
+      <section className="category-header">
+        <h2 className="category-title">{category}</h2>
+        <SearchBar searchHandler={props.searchHandler} />
+      </section>
+      <section className="cards-wrap">
+        <div className="cards">{displayCards(props.zoo[category].filter((el) => el.name.toLowerCase().includes(props.search.toLowerCase())).length)}</div>
+      </section>
+    </>
   );
 }
 
