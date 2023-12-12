ea9dad4c972a09d20ecf4200048034122d721719
Dóra Tokai <tokaidora88@gmail.com>
2023-12-12 11:03:27 +0200

    Fix redundant alt attribute on image
    https://sonarcloud.io/project/issues?open=AYxdO706n2WYUVl9___V&id=tdora28_zoo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Card = ({ name, likes, category, decreaseLikes, increaseLikes, removeCard }) => {
   return (
@@ -7,7 +7,7 @@ const Card = ({ name, likes, category, decreaseLikes, increaseLikes, removeCard 
         <span className="material-symbols-outlined">close</span>
       </button>
       <div className="card__img">
-        <img src={`https://source.unsplash.com/400x400/?${name}`} alt={`unsplash image of ${name}`} />
+        <img src={`https://source.unsplash.com/400x400/?${name}`} alt={`${name}`} />
       </div>
       <div className="card__content">
         <h2 className="card__title">{name}</h2>
@@ -16,7 +16,7 @@ const Card = ({ name, likes, category, decreaseLikes, increaseLikes, removeCard 
             <span className="material-symbols-outlined">remove</span>
           </button>
           <p className="card__rating-score">
-            <span className="material-symbols-outlined">{likes >= 0 ? 'favorite' : 'heart_broken'}</span> <span>{likes}</span>
+            <span className="material-symbols-outlined">{likes >= 0 ? "favorite" : "heart_broken"}</span> <span>{likes}</span>
           </p>
           <button className="card__rating-btn" onClick={increaseLikes}>
             <span className="material-symbols-outlined">add</span>
